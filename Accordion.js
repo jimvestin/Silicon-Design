942413a7847a52af81680a1521c7271894a57ae1
Buster-coder <gokhan_89@hotmail.com>
2026-09-22 17:24:57 +0200

    Update Accordion.js

--- Accordion.js
+++ Accordion.js
@@ -1,8 +1,14 @@
+//Gökhan accordion
+
 document.addEventListener("DOMContentLoaded", function () {
     let lastChecked = null; 
 
+
+    // Ansluter till html klassen "input-radio"
     const radioButtons = document.querySelectorAll('.input-radio');
 
+
+    // Det som gör att det klick functionen går igång
     radioButtons.forEach(radio => {
         radio.addEventListener('click', function () {
             
