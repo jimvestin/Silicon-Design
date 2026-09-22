92e12e5e78ebf9410f55793cfae9e681981ee8f5
Buster-coder <gokhan_89@hotmail.com>
2026-09-22 18:12:31 +0200

    Update Accordion.js

--- Accordion.js
+++ Accordion.js
@@ -12,11 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
     radioButtons.forEach(radio => {
         radio.addEventListener('click', function () {
             
+
+            //Går igång ifall jag klickar på samma radio 2 gånger, tex A sen klickar A igen
             if (radio === lastChecked) {
                 radio.checked = false;
                 lastChecked = null; 
             } else {
                 
+                //Går igång ifall jag går från radio A till radio B, så denna delen hanterar byten
                 lastChecked = radio;
             }
         });
